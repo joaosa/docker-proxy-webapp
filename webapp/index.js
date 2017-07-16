c3440c55f2080ac7744fcd10fa218dcd09633937
Joao Andrade <jjadonotenter@gmail.com>
2017-07-16 18:30:00 +0100

    Add base webapp

---
+++ webapp/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  const html = `
+    <section>
+    <h1>I'm a webapp</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </section>
+  `;
+  res.end(html);
+}).listen(8080);
